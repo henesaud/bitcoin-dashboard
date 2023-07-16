68d246955036de60b5e89d4c3211055fec86c23b
henesaud <hstuchisaud@gmail.com>
2023-07-16 00:08:20 -0300

    removing postgres integration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -65,13 +65,9 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 # Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

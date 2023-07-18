51fea47640fd470c2ffe7fa13fa023f703ea3453
henesaud <hstuchisaud@gmail.com>
2023-07-18 20:42:07 -0300

    Changing main menu & creating router & creating login

---
+++ frontend/src/components/MainDashboard.tsx
@@ -0,0 +1,69 @@
+import React from "react"
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Chart from "./Chart";
+
+const MainDashboard: React.FC = () => {
+    return <>
+        <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <Box
+                component="main"
+                sx={{
+                    backgroundColor: (theme) =>
+                        theme.palette.mode === "light"
+                            ? theme.palette.grey[100]
+                            : theme.palette.grey[900],
+                    flexGrow: 1,
+                    height: "100vh",
+                    overflow: "auto",
+                }}
+            >
+                <Toolbar />
+                <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} md={8} lg={9}>
+                            <Paper
+                                sx={{
+                                    p: 2,
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    height: 240,
+                                }}
+                            >
+                                <Chart />
+                                {/* <component here /> */}
+                            </Paper>
+                        </Grid>
+
+                        <Grid item xs={12} md={4} lg={3}>
+                            <Paper
+                                sx={{
+                                    p: 2,
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    height: 240,
+                                }}
+                            >
+                                {/* <component here /> */}
+                            </Paper>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+                                {/* <comonent here /> */}
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+        </Box>
+
+    </>
+}
+
+export default MainDashboard

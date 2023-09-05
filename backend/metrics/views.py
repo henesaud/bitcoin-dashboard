5058451d5a375586bb85036c77494d31bc714a4f
henesaud <hstuchisaud@gmail.com>
2023-09-05 10:49:25 -0300

    refactoring chart generations

--- backend/metrics/views.py
+++ backend/metrics/views.py
@@ -5,6 +5,20 @@ from rest_framework import status as drf_status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+MAYER_MULTIPLE_DAYS = 200
+
+
+def generate_price_chart_data(prices_data, params):
+    mayer_days = params["mayer_multiple_days"]
+    days = int(params["days"])
+    selected_period_prices = []
+    for price in prices_data[mayer_days - days :]:
+        timestamp = price[0]
+        date_from_timestamp = datetime.datetime.fromtimestamp(timestamp / 1e3)
+        x_axix = date_from_timestamp.strftime("%m/%d")
+        y_axix = round(price[1], 2)
+        selected_period_prices.append([x_axix, y_axix])
+    return selected_period_prices
 
 class BitcoinMetrics(APIView):
     def get(self, *args, **kwargs):
@@ -26,6 +40,10 @@ class BitcoinMetrics(APIView):
                 price[0] = date_from_timestamp.strftime("%m/%d")
                 price[1] = round(price[1], 2)
 
+            prices_chart_data = generate_price_chart_data(
+                prices_data=prices,
+                params={**query_params, "mayer_multiple_days": MAYER_MULTIPLE_DAYS},
+            )
             last_total_volume = (
                 chart_data_json["total_volumes"].pop()
                 if chart_data_json["total_volumes"]
@@ -36,9 +54,10 @@ class BitcoinMetrics(APIView):
                 if chart_data_json["market_caps"]
                 else None
             )
+
             return Response(
                 data={
-                    "prices": prices,
+                    "prices": prices_chart_data,
                     "total_volume": last_total_volume[1],
                     "market_cap": last_market_cap[1],
                 },
